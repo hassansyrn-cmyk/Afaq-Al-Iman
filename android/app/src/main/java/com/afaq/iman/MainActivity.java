14130995485c658500064a246504e197479cf016
hassansyrn-cmyk <hassan.syrn@gmail.com>
2026-07-31 19:03:46 +0400

    v5

--- android/app/src/main/java/com/afaq/iman/MainActivity.java
+++ android/app/src/main/java/com/afaq/iman/MainActivity.java
@@ -1,5 +1,5 @@
 package com.afaq.iman;
-
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity { protected void onCreate(Bundle b){ super.onCreate(b); WindowCompat.setDecorFitsSystemWindows(getWindow(), true); } }
